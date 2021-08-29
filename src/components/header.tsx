968ea8c6a36d4400de09eef26bf9765b36441fcd
SeongEunPark <sungeun1214@gmail.com>
2021-08-29 15:46:42 +0900

    Implement withStyles component, update styles
    - Implement withStyles component (styled-mui-components/Button)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,18 @@
-import { Button } from '@material-ui/core';
 import React from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
-import './header.scss'
+import styles from './header.module.scss';
+import Button from '../styled-mui-components/Button';
 
 const Header = () => {
-  
 
   return (
-    <div className="header">
-      <Button>
-        <MenuIcon />
+    <div className={styles.header}>
+      <Button className={styles.menu} variant="contained" color="primary">
+        <MenuIcon style={{fontSize: '36px'}} />
       </Button>
+      <div className={styles.title_area}>
+        <p className={styles.title}><span className="icon">💪</span> Daily tasks </p>
+      </div>
     </div>
   )
 }
---
+++ src/styled-mui-components/Button.tsx
@@ -0,0 +1,9 @@
+import { Button, withStyles } from '@material-ui/core';
+
+const style = {
+  root: {
+    borderRadius: '0px',
+  },
+}
+
+export default withStyles(style)(Button)
