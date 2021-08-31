04bcfe5b6fc64a6e2774b34554d71518c66a0d30
SeongEunPark <sungeun1214@gmail.com>
2021-09-01 01:26:36 +0900

    Implement styled ListItem component

---
+++ src/styled-mui-components/ListItem.tsx
@@ -0,0 +1,11 @@
+import { ListItem, withStyles } from '@material-ui/core';
+
+const style = {
+  root: {
+    height: '70px',
+    lineHeight: '70px',
+    padding: '0px',
+  },
+}
+
+export default withStyles(style)(ListItem)
